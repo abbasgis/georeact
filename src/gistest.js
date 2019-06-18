88a11aee1d5d316cdfd45abaa7eaf656b7795ea5
abbasgis <abbasgis@gmail.com>
2019-06-18 03:27:44 -0700

    Use of Redux

--- src/gistest.js
+++ src/gistest.js
@@ -1,10 +1,23 @@
-import Map2D from "./components/Map2D";
-import * as React from 'react';
-import * as ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import MapPanel from "./components/MapPanel";
+import reducer from "./reducers/GISTestReducer";
+import {Provider} from "react-redux";
+
+// import reducer from "./store/GISTestStore";
 
 
+export const store = createStore(reducer);
 
 const mountElem = document.getElementById("map-row");
-ReactDOM.render(<Map2D extent={[
-    6734829.193000, 2692598.219300, 8849899.518100, 4509031.393100
-]} zoomLevel={5}/>,mountElem);
+
+// ReactDOM.render(<Map2D extent={[
+//     6734829.193000, 2692598.219300, 8849899.518100, 4509031.393100
+// ]} zoomLevel={5}/>, mountElem)
+
+// let mapPanelRender = () => ReactDOM.render(<MapPanel store={store} />, mountElem);
+// mapPanelRender();
+// store.subscribe(mapPanelRender);
+
+ReactDOM.render(<Provider store={store}><MapPanel/></Provider>, mountElem)
